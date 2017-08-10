44ae1892ebf95630c443e9ae05ab79b6652b159a
clancyz <clancyzhu@vip.qq.com>
2017-08-10 14:43:02 +0800

    update templates

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,30 +1,46 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>\{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h1>\{{ greeting }}</h1>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li>
+        <x-button type="primary" @click="welcome"> Hello World </x-button>
+      </li>
+      <li>
+        Xcui is a collection of Vue.js 2.x desktop components collection.
+      </li>
+      <li>
+        To learn more about xcui, visit
+        <a :href="docsURL" target="_blank">
+          \{{ humanizeURL(docsURL) }}
+        </a>
+      </li>
+      <li>
+        Github:
+        <a :href="docsURL" target="_blank">
+          \{{ humanizeURL(repoURL) }}
+        </a>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  el: '#app',
+  data: {
+    greeting: 'Welcome to your Vue.js & xcui app!',
+    docsURL: 'https://wmfe.github.io/xcui/',
+    repoURL: 'https://github.com/wmfe/xcui',
+  },
+  methods: {
+    humanizeURL: function (url) {
+      return url
+        .replace(/^https?:\/\//, '')
+        .replace(/\/$/, '')
+    },
+    welcome() {
+      this.$Message.info('Hello World!')
     }
   }
 }
@@ -32,12 +48,12 @@ export default {
 
 <style{{#sass}} lang="scss"{{/sass}}>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family:  Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 80px;
 }
 
 h1, h2 {
@@ -50,8 +66,8 @@ ul {
 }
 
 li {
-  display: inline-block;
-  margin: 0 10px;
+  display: block;
+  margin: 15px;
 }
 
 a {
--- template/src/main.js
+++ template/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
+import xcui from 'xcui'
+import 'xcui/lib/xcui.css'
 import App from './App.vue'
+
+Vue.use(xcui)
 
 new Vue({
   el: '#app',
